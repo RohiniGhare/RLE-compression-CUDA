e6532505c4565a3175ebf1d3167de87cbdb7662d
buensons <damian.roszczyk@gmail.com>
2020-01-31 13:33:37 +0100

    awdaw

--- rle.cu
+++ rle.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 __global__ void backWardMask(char * input, int * mask, int n) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
@@ -17,19 +18,22 @@ __global__ void backWardMask(char * input, int * mask, int n) {
 
 // NVIDIA's upsweep and downsweep prefix sum (prescan)
 // TO-DO -> eliminate bank conflicts
-__global__ void prescan(int *g_odata, int *g_idata, int n) {
+__global__ void prescan(int *g_odata, int *g_idata, int * blockSums, int n) {
     extern __shared__ int temp[];
     int thid = threadIdx.x; 
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int index = 2 * blockIdx.x * blockDim.x + threadIdx.x;
     int offset = 1; 
-    temp[thid] = 0;
+
+    temp[2*thid] = 0;
+    temp[2*thid+1] = 0;
 
     if(index < n) {
-        temp[thid] = g_idata[index];
-        temp[thid+1] = g_idata[index+1];
+        temp[2*thid] = g_idata[index];
+        temp[2*thid+1] = g_idata[index+1];
     }
 
-    for (int d = blockDim.x>>1; d > 0; d >>= 1) {   // build sum in place up the tree 
+    // build sum in place up the tree 
+    for (int d = 2*blockDim.x>>1; d > 0; d >>= 1) {
         __syncthreads();    
         
         if (thid < d) {
@@ -37,12 +41,17 @@ __global__ void prescan(int *g_odata, int *g_idata, int n) {
             int bi = offset*(2*thid+2)-1;
             temp[bi] += temp[ai];
         }    
-        offset *= 2; 
+        offset *= 2;
     } 
 
-    if (thid == 0) { temp[blockDim.x - 1] = 0; } // clear the last element
+    // clear the last element
+    if (thid == 0) {
+        blockSums[blockIdx.x] = temp[2*blockDim.x - 1]
+        temp[2*blockDim.x - 1] = 0; 
+    }
 
-    for (int d = 1; d < blockDim.x; d *= 2) { // traverse down tree & build scan     
+    // traverse down tree & build scan  
+    for (int d = 1; d < 2*blockDim.x; d *= 2) {   
         offset >>= 1;      
         __syncthreads();      
         if (thid < d) {
@@ -52,11 +61,29 @@ __global__ void prescan(int *g_odata, int *g_idata, int n) {
             temp[ai] = temp[bi]; 
             temp[bi] += t;       
         } 
-    }  
+    }
     __syncthreads();
 
-    g_odata[index] = temp[thid];  
-    g_odata[index+1] = temp[thid+1]; 
+    temp[2*thid] = temp[2*thid + 1]
+    
+    if(thid == blockDim.x - 1) {
+        temp[2*thid + 1] = blockSums[blockIdx.x];
+    } else {
+        temp[2*thid + 1] = temp[2*thid + 2]
+    }
+
+    g_odata[index] = temp[2*thid];  
+    g_odata[index+1] = temp[2*thid+1];
+}
+
+__global__ void addOffsets(int * preScannedMask, int * blockScan) {
+
+    int index = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if(blockIdx.x == 0) return;
+
+    preScannedMask[index] += blockScan[blockIdx.x-1];
+
 }
 
 __global__ void compactKernel(int * scannedMask, int * compactedMask, int * totalRuns, int n) {
@@ -99,26 +126,38 @@ __global__ void scatterKernel(int * compactedMask, int * totalRuns, int * in, in
 int main(int argc, char ** argv) {
 
     char * in = argv[1];
-    int input_size = sizeof(argv[1])/sizeof(argv[0]);
-    int * mask = new int[input_size];
-    int * scannedMask = new int[input_size];
-    int * compactedMask = new int[input_size];
-    int * totalRuns = new int;
+    int input_size = (int)strlen(argv[1])
 
-    cudaMallocManaged(&in, input_size * sizeof(char));
+    // GPU variables
+    int * mask, * scannedMask, compactedMask;
+    int * totalRuns;
+    char * input;
+    int * block_sums, * scannedBlockSums;
+    int gridSize = (input_size + 1024 - 1) / 1024;
+
+    cudaMallocManaged(&input, input_size * sizeof(char));
     cudaMallocManaged(&mask, input_size * sizeof(int));
     cudaMallocManaged(&scannedMask, input_size * sizeof(int));
+    cudaMallocManaged(&block_sums, gridSize * sizeof(int));
+    cudaMallocManaged(&scannedBlockSums, gridSize * sizeof(int));
 
-    int blockSize; 
-    int minGridSize;
+    cudaMemcpy(input, in, input_size * sizeof(char), cudaMemcpyHostToDevice);
 
-    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, prescan, 0, 0);
-    int gridSize = (input_size + blockSize - 1) / blockSize;
-
-    backWardMask<<<8, 512>>>(in, mask, input_size);
+    backWardMask<<<8, 1024>>>(in, mask, input_size);
     cudaDeviceSynchronize();
-    prescan<<<gridSize, blockSize>>>(scannedMask, mask, input_size);
+    prescan<<<gridSize, 1024>>>(scannedMask, mask, input_size);
     cudaDeviceSynchronize();
+
+    if(input_size > 2048) {
+        
+        // scan of block scans
+        prescan<<<1,ceil(gridSize)>>>(scannedBlockSums, block_sums, gridSize);
+        cudaDeviceSynchronize();
+
+        // add the offset
+        addOffsets<<<gridSize, 2048>>>(scannedMask, scannedBlockSums);
+    }
+
     // compactKernel<<<blocks, 512>>>();
     // cudaDeviceSynchronize();
     // scatterKernel<<<,>>>();
@@ -137,6 +176,4 @@ int main(int argc, char ** argv) {
     }
     std::cout << endl;
     
-    delete [] mask;
-    delete [] scannedMask;
 }
