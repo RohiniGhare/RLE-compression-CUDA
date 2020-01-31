0d884fcf058676d164a7f1c6753dfa4a648e24fc
buensons <damian.roszczyk@gmail.com>
2020-01-31 13:59:45 +0100

    awdaw

--- rle.cu
+++ rle.cu
@@ -8,10 +8,10 @@ __global__ void backWardMask(char * input, int * mask, int n) {
 
     if(index == 0) {
         mask[0] = 1;
-        return;
+        return; // ?
     }
 
-    for(int i = index; i < n - 1; i+=stride) {
+    for(int i = index; i < n; i+=stride) {
         mask[i] = input[i] == input[i - 1] ? 0 : 1;
     }
 }
@@ -147,7 +147,9 @@ int main(int argc, char ** argv) {
     cudaMallocManaged(&bs, gridSize * sizeof(int));
 
     cudaMemcpy(input, in, input_size * sizeof(char), cudaMemcpyHostToDevice);
-
+    for(int i = 0; i < input_size; i++) {
+        std::cout << input[i] << " ";
+    }
     backWardMask<<<8, 1024>>>(in, mask, input_size);
     cudaDeviceSynchronize();
     for(int i = 0; i < input_size; i++) {
